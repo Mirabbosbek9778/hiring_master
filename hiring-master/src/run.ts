ad8d65d2fa4383e6cd74c245f059cb4a69136d18
Mirabbosbek9778 <jwicj969@gmail.com>
2023-08-01 16:10:30 +0500

    task

--- hiring-master/src/run.ts
+++ hiring-master/src/run.ts
@@ -1,10 +1,57 @@
-import { IExecutor } from "./Executor";
-import ITask from "./Task";
+import { Task } from "./Task";
+import { Executor, IExecutor } from "./Executor";
 
-export default async function run(
-  executor: IExecutor,
-  queue: AsyncIterable<ITask>,
-  maxThreads = 0
-) {
-  maxThreads = Math.max(0, maxThreads);
+async function run(queue: Iterable<Task>, maxThreads = 0): Promise<void> {
+  const executor: IExecutor = new Executor();
+  const targetIdMap: Map<number, Task[]> = new Map();
+
+  const processTask = async (task: Task) => {
+    const targetIdTasks = targetIdMap.get(task.targetId) || [];
+    if (targetIdTasks.length > 0) {
+      await targetIdTasks[targetIdTasks.length - 1].executionPromise;
+    }
+    targetIdTasks.push(task);
+    targetIdMap.set(task.targetId, targetIdTasks);
+
+    await executor.executeTask(task);
+
+    if (targetIdTasks.length > 0 && targetIdTasks[0] === task) {
+      targetIdTasks.shift();
+    }
+  };
+
+  const processQueue = async () => {
+    const queueIterator = queue[Symbol.iterator]();
+    let activeThreads = 0;
+
+    const processNextTask = async () => {
+      const nextTask = queueIterator.next();
+      if (!nextTask.done) {
+        const task = nextTask.value;
+        activeThreads++;
+
+        processTask(task)
+          .then(() => {
+            activeThreads--;
+            processNextTask();
+          })
+          .catch((error) => {
+            activeThreads--;
+            processNextTask();
+          });
+      }
+    };
+
+    while (activeThreads < maxThreads || maxThreads === 0) {
+      processNextTask();
+      if (activeThreads === 0) {
+        break;
+      }
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    }
+  };
+
+  await processQueue();
 }
+
+export { run };
